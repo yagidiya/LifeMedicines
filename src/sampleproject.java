0a23464ddf6fae1472ca0f2b07939b6887b480ec
yagidiya <divyapalani2994@gmail.com>
2025-06-20 12:53:23 +0530

    Test case created

---
+++ src/sampleproject.java
@@ -0,0 +1,16 @@
+
+public class sampleproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="Life Science Project";
+		System.out.println(s);
+
+       
+    }
+}
+
+	
+
+
